f28617dbf32e697993f131d01c890df10f0394e4
doolri1276 <doolri1277@gmail.com>
2019-03-19 16:32:17 +0900

    - Angular import받기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { LangSelectorComponent } from './lang-selector/lang-selector.component';
 import { FormsModule, COMPOSITION_BUFFER_MODE } from '@angular/forms'
 import { I18nSupportService } from './i18n-support.service';
 import { LangSelectorBtnPipe } from './lang-selector/lang-selector-btn.pipe';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatSnackBarModule, MatCardModule, MatInputModule, MatRadioModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,10 @@ import { LangSelectorBtnPipe } from './lang-selector/lang-selector-btn.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatSnackBarModule, MatCardModule, MatInputModule, MatRadioModule, MatButtonModule
   ],
   providers: [
     {provide: COMPOSITION_BUFFER_MODE, useValue:false},
