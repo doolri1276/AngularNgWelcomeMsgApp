32ff853b3fb6d07a30d76ac854d03e119e0498fc
doolri1276 <doolri1277@gmail.com>
2019-03-19 13:05:39 +0900

    - 예제 3-5 AfterViewInit, onKeyUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
 	title = 'ng-welcome-msg-app';
 	userName="";
+
+	private valid = false;
+	private static CHK_KEYUP_WAIT_SEC = 5000;
+
+	ngAfterViewInit() {
+		const checkTouchFn = () => {
+			if(this.valid) return;
+			alert('이름을 입력해 주세요');
+		}
+
+		setTimeout(checkTouchFn, AppComponent.CHK_KEYUP_WAIT_SEC);
+	}
+
+	onKeyUp(name) {
+		this.valid = name.length > 0;
+	}
 
 	setName(name){
 		this.userName=name;
