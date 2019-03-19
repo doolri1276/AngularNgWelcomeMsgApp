453c8eaa8f853d96135f15f4bdcb6567242690a0
doolri1276 <doolri1277@gmail.com>
2019-03-19 13:47:04 +0900

    - 예제 3-11

--- src/app/i18n-support.service.ts
+++ src/app/i18n-support.service.ts
@@ -4,6 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class I18nSupportService {
+	private welcomeMsg;
 
-  constructor() { }
+  constructor() { 
+  	this.welcomeMsg = {
+		'ko': '안녕하세요',
+		'en': 'Hello',
+		'jp': 'こんにちは',
+		'fr': 'Bonjour',
+		'ch': '你好'
+	}
+  }
+
+  getWelcomeMsgByCode(userName: string, code: string){
+  	const helloMsg = this.welcomeMsg[code];
+  	return `${helloMsg}, ${userName}`;
+  }
 }
