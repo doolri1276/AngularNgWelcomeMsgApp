8781c203614b60fa6755360ad244561428026406
doolri1276 <doolri1277@gmail.com>
2019-03-19 12:54:38 +0900

    - 예제3-3  setName메소드 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-welcome-msg-app';
+	title = 'ng-welcome-msg-app';
+	userName='';
+
+	setName(name){
+		this.userName=name;
+	}
 }
