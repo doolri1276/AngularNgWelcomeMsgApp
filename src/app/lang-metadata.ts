700fe435bc3c96fd188512cd85f6a707a3b6719d
doolri1276 <doolri1277@gmail.com>
2019-03-19 15:33:48 +0900

    - 예제 3-17 JSON객체 선언

---
+++ src/app/lang-metadata.ts
@@ -0,0 +1,29 @@
+export const LANG_METADATA = [
+	{
+		code: 'ko',
+		name: '한국어',
+		flagImgName: 'South_Korea.png',
+		msg: '안녕하세요'
+	},{
+		code: 'en',
+		name: '영어',
+		flagImgName: 'United_States.png',
+		msg: 'Hello'
+	},{
+		code: 'jp',
+		name: '일본어',
+		flagImgName: 'Japan.png',
+		msg: 'こんにちは'
+	},{
+		code: 'fr',
+		name: '프랑스어',
+		flagImgName: 'France.png',
+		msg: 'Bonjour'
+	},{
+		code: 'ch',
+		name: '중국어',
+		flagImgName: 'China.png',
+		msg: '你好'
+	}
+
+]
